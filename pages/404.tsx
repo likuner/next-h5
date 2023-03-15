d5c0a0f2006654d2277b3c9f8f314af280e0a960
likun <273676636@qq.com>
2023-03-15 17:40:12 +0800

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error404: React.FC = () => {
+  return (
+    <div style={{textAlign:'center', marginTop: '100px'}}>404 - 当前页面不存在</div>
+  )
+}
+
+export default Error404
