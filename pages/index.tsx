869f017090b4666f3e2f18bb478d6e49b3ec22ac
likun <273676636@qq.com>
2023-03-17 18:20:21 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useState } from 'react'
+
 export default function Home() {
+  const [products, setProducts] = useState([])
+
+  const router = useRouter()
+  
+  const fetchData = useCallback(async () => {
+    const res = await fetch('https://dummyjson.com/products')
+    const { products } = await res.json()
+    setProducts(products)
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const handleClick = (e: any) => {
+    router.push(`/products/${e.id}`)
+  }
+
   return (
-    <div>Index Page</div>
+    <ul>
+      <li>Index Page</li>
+      {products.map((item: any) => 
+        <li key={item.id} onClick={() => handleClick(item)}>{item.title}</li>
+      )}
+    </ul>
   )
 }
