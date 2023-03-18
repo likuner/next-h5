74a24e3b7cbd908f3b9099fa6631230abb423f1b
likun <273676636@qq.com>
2023-03-18 20:38:34 +0800

    update

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest, event: NextFetchEvent) {
-  fetch('http://localhost:3000/api/saveLog', {
-    method: 'post',
-    body: JSON.stringify(['middleware', 'route', req.nextUrl.pathname])
-  })
+  // fetch('http://localhost:3000/api/saveLog', {
+  //   method: 'post',
+  //   body: JSON.stringify(['middleware', 'route', req.nextUrl.pathname])
+  // })
   return NextResponse.next()
 }
 
